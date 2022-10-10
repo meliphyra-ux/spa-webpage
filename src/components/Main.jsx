08cbc7c889ff38953f12c1e449f6db5dbd325ba9
meliphyra-ux <xennerit@icloud.com>
2022-10-10 14:28:44 +0300

    Completed header and first section

---
+++ src/components/Main.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import styles from '../assets/styleModules/main.module.css'
+
+const Main = () => {
+  return (
+    <section className={styles.main}>
+        <article>
+            <h1>Content marketing for startups</h1>
+            <p>Get high-quality content that drives organic traffic to your blog, by boosting your DA.</p>
+            <button>Get Started</button>
+        </article>
+    </section>
+  )
+}
+
+export default Main
