fc4ffefe5561f33c27af5abaca0e9584e8546656
meliphyra-ux <xennerit@icloud.com>
2022-10-12 18:15:46 +0300

    Added another section

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import Header from './components/Header';
 import Main from './components/Main';
 import CardBlock from './components/CardBlock'
 import ResultBlock from './components/ResultBlock';
+import Marketing from "./components/Marketing";
 
 function App() {
   return (
@@ -12,6 +13,7 @@ function App() {
         <Main />
         <CardBlock />
         <ResultBlock />
+        <Marketing />
     </main>
   );
 }
---
+++ src/components/Marketing.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import styles from "../assets/styleModules/marketing.module.css"
+
+const Marketing = () => {
+    return (
+        <section className={styles.marketing}>
+            <article>
+                <h1>What you get with our content marketing for startups service</h1>
+                <p>List a product once and it will be published across all of your connected sales channels
+                    . Similarly,
+                    when you make changes to your listing</p>
+            </article>
+        </section>
+    )
+}
+export default Marketing
