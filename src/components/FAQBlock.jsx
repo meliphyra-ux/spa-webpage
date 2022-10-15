4dca3c72990c7d7a0deea05f2f34744516e183dc
meliphyra-ux <xennerit@icloud.com>
2022-10-15 19:38:00 +0300

    Added styles to FAQBlock

--- src/components/FAQBlock.jsx
+++ src/components/FAQBlock.jsx
@@ -1,11 +1,12 @@
 import React, {useState} from "react";
 import Open from "../assets/images/open.svg"
 import Close from "../assets/images/close.svg"
+import styles from "../assets/styleModules/FAQBlock.module.css"
 
 const FAQBlock = ({title, text}) => {
     const [isOpen, setIsOpen] = useState(false)
     return (
-        <figure>
+        <figure className={styles.FAQBlock}>
             <img src={isOpen ? Close : Open} width={30} height={30} alt="Open/Close button"
                  onClick={() => setIsOpen(!isOpen)}/>
             <h2>{title}</h2>
