a6b68feb28924582901f473e6af510620057d52f
meliphyra-ux <xennerit@icloud.com>
2022-10-19 22:41:37 +0300

    Finally fixed background image

--- src/components/FAQ.jsx
+++ src/components/FAQ.jsx
@@ -1,9 +1,10 @@
 import React from "react";
 import FAQBlock from "./FAQBlock";
+import styles from "../assets/styleModules/FAQ.module.css"
 
 const FAQ = () => {
     return(
-        <section>
+        <section className={styles.FAQ}>
             <FAQBlock title="How long until we deliver your first blog" text="123"/>
             <FAQBlock title="How long until we deliver your first blog" text="123"/>
             <FAQBlock title="How long until we deliver your first blog" text="123"/>
